4f50b94e29ccc99b31f6d2a28601d757c73e93f7
Md. Mehedi Hasan <74109996+Mehedihasan8231@users.noreply.github.com>
2024-06-07 08:35:01 +0600

    Create Fectorial.java
    create a java file Fectorial.java

---
+++ Fectorial.java
@@ -0,0 +1,13 @@
+public class Fectorial{
+  public static int factorial(int n){
+  if(n==1){
+    return 1;
+    
+  }
+  return n*factorial(n-1);
+}
+public static void main(String[] argu){
+  int result=factorial(5);
+  System.out.println(result);
+}
+}
